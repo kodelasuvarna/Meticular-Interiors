a5920c23fd88e8fafaf004f71dc229c842a22407
Kumarrayal123 <kardamkumar13@gmail.com>
2025-06-04 18:55:51 +0530

    kk

--- assets/js/sendemail.php
+++ assets/js/sendemail.php
@@ -13,7 +13,7 @@ try {
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'hello@homezointeriors.com'; // Your Gmail
+    $mail->Username = 'kardamkumar13@gmail.com'; // Your Gmail
     $mail->Password = 'your_app_password';   // Gmail App Password
     $mail->SMTPSecure = 'tls';
     $mail->Port = 587;
